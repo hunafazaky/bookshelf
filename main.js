bfa8d00bc4c2dfcfb63e2e09fcaa937567584765
hunafazaky <hunafa.zsn@gmail.com>
2026-04-04 21:36:45 +0700

    minor update

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 // Do your work here...
-// console.log('Hello, world!');
+console.log("Hello, world!");
+document
+  .getElementById("bookForm")
+  .addEventListener("submit", function (event) {});
